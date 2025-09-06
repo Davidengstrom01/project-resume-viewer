15a7d2bdf137f77bc5da27e76493964299ddc385
Davidengstrom01 <davveengstrom01@gmail.com>
2025-09-06 21:33:40 +0200

    redid home component

--- project-resume-viewer/src/app/app.routes.ts
+++ project-resume-viewer/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { HomePageComponent } from './features/home/pages/home-page/home-page.component';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
-  { path: '', component: HomePageComponent },
+  { path: '', component: Home },
   { path: '**', redirectTo: '' }
 ];
--- project-resume-viewer/src/main.ts
+++ project-resume-viewer/src/main.ts
@@ -1,20 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { APP_INITIALIZER } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
 import { App } from './app/app';
 import { routes } from './app/app.routes';
-import { RepoStore } from './app/core/stores/repo.store';
 
 bootstrapApplication(App, {
   providers: [
-    provideRouter(routes),
-    provideHttpClient(),
-    {
-      provide: APP_INITIALIZER,
-      multi: true,
-      deps: [RepoStore],
-      useFactory: (repoStore: RepoStore) => () => repoStore.prefetch()
-    }
+    provideRouter(routes)
   ]
 }).catch((err) => console.error(err));
