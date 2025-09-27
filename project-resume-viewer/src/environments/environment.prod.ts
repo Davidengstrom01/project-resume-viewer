23edff064f1dac0d2c4b4bc14ccf76c6ab036af8
David Engström <davveengstrom01@gmail.com>
2025-09-27 18:01:51 +0200

    FIXED URL

--- project-resume-viewer/src/environments/environment.prod.ts
+++ project-resume-viewer/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   production: true,
   githubUsername: 'your-username',
   githubToken: undefined as string | undefined,
-  apiUrl: 'https://projectresumeviewerapi-g2enazfxabb9gfe7.westeurope-01.azurewebsites.net/swagger/api/Github'
+  apiUrl: 'https://projectresumeviewerapi-g2enazfxabb9gfe7.westeurope-01.azurewebsites.net/api/Github'
 };
