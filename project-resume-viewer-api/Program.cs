bba615ffbd548134e0d967b9d93eeae0ef3c79ce
David Engström <davveengstrom01@gmail.com>
2025-09-28 14:15:07 +0200

    fix cors and headers

--- project-resume-viewer-api/Program.cs
+++ project-resume-viewer-api/Program.cs
@@ -7,15 +7,15 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient();
 
 const string FrontendCors = "FrontendCors";
-builder.Services.AddCors(o =>
-    o.AddPolicy(FrontendCors, p =>
-        p.WithOrigins(
-            "http://localhost:65043",
-            "http://127.0.0.1:65043", 
-            "https://gray-river-079a4881e.1.azurestaticapps.net"
-        )
-        .AllowAnyHeader()
-        .AllowAnyMethod()));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(FrontendCors, policy =>
+    {
+        policy.WithOrigins("https://gray-river-079a4881e.1.azurestaticapps.net")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
